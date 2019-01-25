4d1db4eeac0845ced4a088c47a16bffd13124368
Paul Pop <paulpop88@gmail.com>
2019-01-25 01:16:57 +0000

    Add Mongoose and connect to DB Add docker compose setup

--- lab-3-todo-app/todo-api/app/index.js
+++ lab-3-todo-app/todo-api/app/index.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
 const logger = require("pino")();
+const mongoose = require("mongoose");
 const server = require("./server");
 
 const env = process.env.NODE_ENV || "development";
 const port = process.env.PORT || 8080;
+const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/todo";
+
+mongoose.connect(mongoURI, { useNewUrlParser: true });
 
 server.listen(port, () => {
   logger.info(`Running service on port ${port} using NODE_ENV: ${env}`);

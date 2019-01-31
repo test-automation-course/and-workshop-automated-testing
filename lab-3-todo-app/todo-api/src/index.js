bf02dddac17cbbf539151b9ca8a5d69e2ce45842
Paul Pop <paulpop88@gmail.com>
2019-01-31 13:50:03 +0000

    Move Mongoose init to server.js

--- lab-3-todo-app/todo-api/src/index.js
+++ lab-3-todo-app/todo-api/src/index.js
@@ -1,13 +1,8 @@
 require("dotenv").config();
 const logger = require("pino")();
-const mongoose = require("mongoose");
 const server = require("./server");
 
 const env = process.env.NODE_ENV || "development";
-const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/todos";
-
-mongoose.connect(mongoURI, { useNewUrlParser: true });
-
 server.listen(8080, () => {
   logger.info(`Running service on port 8080 using NODE_ENV: ${env}`);
 });
--- lab-3-todo-app/todo-api/src/server.js
+++ lab-3-todo-app/todo-api/src/server.js
@@ -1,9 +1,13 @@
 const express = require("express")();
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser").json();
 const cors = require("cors")();
 const pino = require("express-pino-logger")();
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
+
+const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/todos";
+mongoose.connect(mongoURI, { useNewUrlParser: true });
 
 express
   .use(bodyParser)

aa9fdf7b7a11b19d58d12caeac41e50f18374c8e
Matt Rosenquist <mrosenquist@and.digital>
2019-01-31 05:47:07 +0000

    Update ugly code to be less ugly (#2)

--- lab-2-improve-code/src/index.js
+++ lab-2-improve-code/src/index.js
@@ -1,1 +1,14 @@
-j = require('./../src/libs'); Object.keys(j).map(i => console.log(i+":", j[i](i=='req-TWO'?null:'YO')));j = require('./../src/libs2'); Object.keys(j).map(i => console.log(i+":", j[i](['Mary', 'Jane', 'Test'])))
+
+module.exports = function greeting(i = 'my friend') {
+  if (typeof i == 'string') {
+    i = [i];
+  }
+
+out = 'Hello, ';
+
+  nStr = '';
+  for (j in i ) { nStr = nStr + i[j] + ((parseInt(j)+2==i.length)?', and ': ((parseInt(j)+1==i.length)?'':', ')); }
+
+    if (nStr.toUpperCase() === nStr) return "HELLO " + nStr +  '!'
+
+  return "Hello, " + nStr +  '.'}
--- lab-2-improve-code/tests.test.js
+++ lab-2-improve-code/tests.test.js
@@ -1,41 +1,33 @@
-const {
-  ["req-one"]: requirement1,
-  ["req-TWO"]: requirement2,
-  ["req-3"]: requirement3
-} = require("./src/libs");
-const {
-  ["req-FOUR"]: requirement4,
-  req_five: requirement5
-} = require("./src/libs2");
+const greeting = require("./src/index");
 
 describe("Greeting Test", () => {
   describe("Requirement 1", () => {
     it("is a func", () => {
-      expect(typeof requirement1).toEqual("function");
+      expect(typeof greeting).toEqual("function");
     });
   });
 
   describe("Requirement 2", () => {
     it("shows correct out put when in put is 'test'", () => {
-      expect(requirement2("test")).toEqual("Hello, test.");
+      expect(greeting("test")).toEqual("Hello, test.");
     });
   });
 
   describe("Requirement 3", () => {
     it("shows correct out put when in put is 'test'", () => {
-      expect(requirement3("test")).toEqual("Hello, test.");
+      expect(greeting("test")).toEqual("Hello, test.");
     });
   });
 
   describe("Requirement 4", () => {
     it("shows correct out put when in put is 'test'", () => {
-      expect(requirement4("test")).toEqual("Hello, test.");
+      expect(greeting("test")).toEqual("Hello, test.");
     });
   });
 
   describe("Requirement 5", () => {
     it("shows correct out put when in put is 'test'", () => {
-      expect(requirement5("test")).toEqual("Hello, test.");
+      expect(greeting("test")).toEqual("Hello, test.");
     });
   });
 });
